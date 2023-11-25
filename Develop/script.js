a15e5113e6e670a876b4e971a934b3897fbecdf9
nickcatalano5 <nickcatalano24@gmail.com>
2023-11-25 16:14:59 -0500

    added four prompts regarding lowercase uppercase numbers and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,15 @@ var specialChar = [" !#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
 
 function generatePassword() {
   var password = "";
-  var passwordAmount = "";
+  var passwordChoices = "";
+  // initial prompt when you enter website
   var passwordLength = prompt(
     "Please input the number of characters you would like your password to be, between 8 - 128."
   );
-  passwordLength = parseInt(passwordLength);
+  // passwordLength = parseInt(passwordLength);
+
+  // error messages if user enters anything other than a number between 8-128
+
   if (passwordLength < 8) {
     alert("Password must be between 8-128!");
     return;
@@ -21,7 +25,34 @@ function generatePassword() {
     return;
   }
 
+  // second prompt for lowercase letters
+
   var lowerCaseOption = confirm("Would you like to include lowercase letters?");
+  if (lowerCaseOption) {
+    // referenced mdn article on the use of += in order to add the option choice into the left var
+    passwordChoices += lowerCaseOption;
+  }
+
+  // third prompt for uppercase letters
+  var upperCaseOption = confirm("Would you like to include uppercase letters?");
+  if (upperCaseOption) {
+    passwordChoices += upperCaseOption;
+  }
+
+  // fourth prompt for numbers
+  var numbersOption = confirm("Would you like to include numbers?");
+  if (numbersOption) {
+    passwordChoices += numbersOption;
+  }
+
+  // fifth prompt for special characters
+  var specialCharOptions = confirm(
+    "Would you like to include special characters?"
+  );
+  if (specialCharOptions) {
+    passwordChoices += specialCharOptions;
+  }
+
   // 1. Prompt the user for the password criteria
   //    a. Password length 8 >= 128
   //    b. Lowercase, uppercase, numbers, special characters
